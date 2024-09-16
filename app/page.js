a75b294a4b611d3e1f932c63bbfc7b2ab43908fb
romain <niamor.lth@gmail.com>
2024-09-16 10:02:28 +0200

    update

--- app/page.js
+++ app/page.js
@@ -1,20 +1,36 @@
+"use client";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const tokenInLocalStorage = localStorage.getItem("token");
+
+    setToken(tokenInLocalStorage);
+  }, []);
+
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <div className="bg-neutral-50 p-8 rounded-lg shadow-lg max-w-md w-full">
-        <h1 className="text-2xl text-gray-700 font-bold mb-6 text-center">
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-full flex flex-col gap-5 ">
+        <h1 className="text-4xl text-gray-700 font-bold mb-6 text-center">
           Bienvenue sur notre application !
         </h1>
-        <div className="flex flex-row justify-evenly h-full">
-          <Link href="/signup" className="text-gray-400">
-            S'inscrire
+        {token ? (
+          <Link href="/signup" className=" mx-auto">
+            Aller au dashboard
           </Link>
-          <Link href="/login" className="text-gray-400">
-            Se connecter
-          </Link>
-        </div>
+        ) : (
+          <div className="flex flex-row justify-center items-center gap-5 h-full">
+            <Link href="/signup" className="">
+              S'inscrire
+            </Link>
+            <Link href="/login" className="">
+              Se connecter
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
